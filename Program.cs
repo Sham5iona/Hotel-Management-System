1c256e6f550b6d494674561f6338b4ac37d82fc7
Sham5iona <107301002+Sham5iona@users.noreply.github.com>
2024-04-08 00:09:31 +0300

    Program.cs
    Here is the Program.cs file. The improved logics are the Identity Services, the dependency injection between ICustomerService and CustomerService. As well as are the status codes middleware, the Auto Mapper service configuration and the identity configuration, and preferences.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+using HMS.Areas.Identity.Model;
+using HMS.Data;
+using HMS.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace HMS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddIdentity<Admin, IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredUniqueChars = 0;
+            })
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddRazorPages();
+
+            builder.Services.AddDbContext<HotelDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+            builder.Services.AddTransient<ICustomerService, CustomerService>();
+            
+            //Add auto mapper here
+            builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
+
+            var app = builder.Build();
+            
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseStatusCodePagesWithReExecute("/Error/{0}");
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+            
+            app.Run();
+        }
+    }
+}
