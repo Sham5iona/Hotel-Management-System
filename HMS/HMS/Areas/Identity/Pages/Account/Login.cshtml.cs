8b8bc228c10304da2cf2248d9ae3ae83536b329b
Sham5iona <gom26@abv.bg>
2024-04-07 23:15:17 +0300

    Here is the 3th commit where I added the Identity Framework and used all the given managers(SignInManager, UserManager and RoleManager).All the validation logic is being configured, data binding and role management! In this project the role management is a bit useless but the target is to demonstrate the ability to work with all the Managers from the Identity Framework Core!

---
+++ HMS/HMS/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,155 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using HMS.Areas.Identity.Model;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace HMS.Areas.Identity.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        private readonly SignInManager<Admin> _signInManager;
+        private readonly ILogger<LoginModel> _logger;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<Admin> _userManager;
+        private readonly IPasswordHasher<Admin> _passwordHasher;
+        public LoginModel(SignInManager<Admin> signInManager,
+                          ILogger<LoginModel> logger,
+                          RoleManager<IdentityRole> roleManager,
+                          UserManager<Admin> userManager,
+                          IPasswordHasher<Admin> passwordHasher)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _passwordHasher = passwordHasher;
+
+        }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public string ReturnUrl { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public class InputModel
+        {
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required]
+            public string Username { get; set; }
+
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Display(Name = "Remember me?")]
+            public bool RememberMe { get; set; }
+        }
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            if (!string.IsNullOrEmpty(ErrorMessage))
+            {
+                ModelState.AddModelError(string.Empty, ErrorMessage);
+            }
+
+            returnUrl ??= Url.Content("~/");
+
+            // Clear the existing external cookie to ensure a clean login process
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+            if (ModelState.IsValid)
+            {
+                // Attempt to sign in the user
+                var result = await _signInManager.PasswordSignInAsync(
+                    Input.Username, Input.Password, Input.RememberMe,
+                    lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    // Retrieve the user by username
+                    var user = await _userManager.FindByNameAsync(
+                                                  Input.Username);
+                    if (user != null)
+                    {
+                        // Check if the user is in the "Administrator" role
+                        bool isAdmin = await _userManager.IsInRoleAsync(user, "Administrator");
+                        if (isAdmin)
+                        {
+                            _logger.LogInformation("User logged in.");
+                            return RedirectToPage("/Index");
+                        }
+                    }
+                    if (result.IsLockedOut)
+                    {
+                        _logger.LogWarning("User account locked out.");
+                        return RedirectToPage("./Lockout");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                        return Page();
+                    }
+                }
+
+                // If we got this far, something failed, redisplay form
+                return Page();
+            }
+            return Page();
+        }
+    }
+}
