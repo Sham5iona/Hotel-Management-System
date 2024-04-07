c2813b2fbe86a101a5cffb45c92fdc9876ae7214
Sham5iona <gom26@abv.bg>
2024-04-07 23:23:36 +0300

    Here are the new migrations and the improved database logic in uniqness of the email in Customers and room number in Rooms columns!

---
+++ HMS/HMS/Data/Migrations/20240405090227_IdentityMigration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HMS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdentityMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "AdminId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdminId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "AspNetUsers");
+        }
+    }
+}
