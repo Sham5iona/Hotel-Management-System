347836082ca737cc709ade659d7aa08ebb535954
Sham5iona <gom26@abv.bg>
2024-04-11 14:47:49 +0300

    Here, I have added a mapper between AdminDTO and Admin and vise versa. And also some comments to remember what to add next time and to keep track.

---
+++ HMS/HMS/Mappings/MappingProfile.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using HMS.Areas.Identity.Model;
+using HMS.DTOs;
+using HMS.Model;
+
+namespace HMS.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //Create AutoM Mapper for Customer and CustomerDTO and vise versa
+            CreateMap<Customer, CustomerDTO>()
+                .ForMember(dest => dest.UserAlreadyExists,
+                           opt => opt.Ignore());
+
+            CreateMap<CustomerDTO, Customer>();
+
+            //Create Auto Mapper for Admin and AdminDTO and vise versa
+
+            CreateMap<AdminDTO, Admin>()
+                .ForMember(dest => dest.PasswordHash,
+                opt => opt.Ignore());
+
+
+            CreateMap<Admin, AdminDTO>()
+                .ForMember(dest => dest.AlreadyExists,
+                opt => opt.Ignore());
+
+            //Reservation: CheckOutDate must be later than CheckInDate.
+
+        }
+    }
+}
