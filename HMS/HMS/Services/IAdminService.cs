4498b2ef54377bd0fedba240a7bcab534ae415d2
Sham5iona <gom26@abv.bg>
2024-04-12 12:01:54 +0300

    Here I have added all the BLL for the Admin Panel by using the Identity Managers mainly! Also, I followed the SOLID principles to make clean, readable and maintable code! The files can be extended because, as I said, I followed the SOLID ones and it won't affect the existing code and business logic!

---
+++ HMS/HMS/Services/IAdminService.cs
@@ -0,0 +1,17 @@
+﻿using HMS.Areas.Identity.Model;
+using HMS.DTOs;
+
+namespace HMS.Services
+{
+    public interface IAdminService
+    {
+        public Task<bool> UpdateAdminAsync(AdminDTO AdminDTO);
+        public Task<bool> DeleteAdminAsync(string id);
+        public IEnumerable<Admin> GetAdmins();
+        public Task<string> GetAdminIDAsync(AdminDTO AdminDTO);
+
+        public Task<bool> CheckIfAlreadyExistsAsync(AdminDTO AdminDTO);
+
+        public Task<Admin> GetAdminByIdAsync(string id);
+    }
+}
