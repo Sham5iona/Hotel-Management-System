926609044ef3b115a07d0cca04bbbf4cec4f5dcc
Sham5iona <107301002+Sham5iona@users.noreply.github.com>
2024-04-20 21:37:04 +0300

    Add CRUD razor pages for Reservations and Rooms
    Here I added all the razor pages for every associated operation on a reservation or a room!

---
+++ Room/EditRoom.cshtml.cs
@@ -0,0 +1,48 @@
+using AutoMapper;
+using HMS.DTOs;
+using HMS.Model;
+using HMS.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HMS.Pages.Room
+{
+    public class EditRoomModel : PageModel
+    {
+        private readonly ILogger<EditRoomModel> _logger;
+        private readonly IRoomService _roomService;
+        private readonly IMapper _mapper;
+        public Model.Room Room { get; set; }
+        [BindProperty]
+        public RoomDTO RoomDTO { get; set; }
+        public EditRoomModel(ILogger<EditRoomModel> logger, IRoomService roomService,
+                             IMapper mapper)
+        {
+            this._logger = logger;
+            this._mapper = mapper;
+            this._roomService = roomService;
+        }
+
+        public async Task<IActionResult> OnPostEditAsync(Guid Id)
+        {
+            Room = await _roomService.GetRoomByIdAsync(Id);
+            RoomDTO = _mapper.Map(Room, RoomDTO);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostEditRoomAsync()
+        {
+            if(ModelState.IsValid)
+            {
+                bool is_succeeded = await _roomService.UpdateRoomAsync(RoomDTO);
+                if(is_succeeded)
+                {
+                    _logger.LogInformation("Successfully updated a room!");
+                    return RedirectToPage("/Room/GetRooms");
+                }
+            }
+
+            return Page();
+        }
+    }
+}
---
+++ Room/GetRooms.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HMS.Pages.Room
+{
+    public class GetRoomsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
